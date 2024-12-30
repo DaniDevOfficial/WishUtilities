ed14ba12c8067c3cad666b1fd056d0047d27eb98
David <bischof.david.db@gmail.com>
2024-12-30 17:02:13 +0100

    fixed home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import { MdToString } from './MdToString'
+import { Flex, Heading } from "@chakra-ui/react";
 
 export function HomePage() {
 
     return (
         <>
+
+        <Flex justifyContent="center" alignItems="center" flexDirection="column">
+            <Heading as="h1"  size="2xl">Welcome to WishUtilities</Heading>
+            <Heading as="h2" size="lg">A collection of utilities for myself</Heading>
+        </Flex>
         </>
     )
 }
